bfe003425cf6af7ce42901712533fe9a6a6f971d
Nugo Bibilashvili <bibilashvili6@gmail.com>
2024-11-04 05:17:37 -0800

    working single page left side

--- src/routes/singlePage/SinglePage.jsx
+++ src/routes/singlePage/SinglePage.jsx
@@ -1,5 +1,6 @@
 import Slider from "../../components/slider/Slider";
 import { singlePostData, userData } from "../../lib/dummydata";
+import Map from "../../components/map/Map";
 
 const SinglePage = () => {
   return (
@@ -35,7 +36,87 @@ const SinglePage = () => {
         </div>
       </div>
       <div className="flex-[2] bg-[#fcf5f3]">
-        <div className="px-[20px]"></div>
+        <div className="px-[20px] flex flex-col gap-[20px]">
+          <p className="font-bold text-lg mb-[10px]">General</p>
+          <div className="flex flex-col gap-[20px] px-[20px] py-[10px] bg-white rounded-[10px]">
+            <div className="flex items-center gap-[10px]">
+              <img src="/utility.png" alt="" className="w-6 h-6 bg-green-500" />
+              <div>
+                <span className="font-bold">Utilities</span>
+                <p className="text-sm">Renter is responsible</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[10px]">
+              <img src="/pet.png" alt="" className="w-6 h-6 bg-green-500" />
+              <div>
+                <span className="font-bold">Pet Policy</span>
+                <p className="text-sm">Pets Allowed</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[10px]">
+              <img src="/fee.png" alt="" className="w-6 h-6 bg-green-500" />
+              <div>
+                <span className="font-bold">Property Fees</span>
+                <p className="text-sm">
+                  Mush have 3x the rent in total household income
+                </p>
+              </div>
+            </div>
+          </div>
+          <p className="font-bold text-lg mb-[10px]">Sizes</p>
+          <div className="flex justify-between">
+            <div className="flex items-center gap-[10px] bg-white p-[10px] rounded-[5px]">
+              <img src="/size.png" alt="" className="w-6 h-6" />
+              <span>80sqft</span>
+            </div>
+            <div className="flex items-center gap-[10px] bg-white p-[10px] rounded-[5px]">
+              <img src="/bed.png" alt="" className="w-6 h-6" />
+              <span>2 beds</span>
+            </div>
+            <div className="flex items-center gap-[10px] bg-white p-[10px] rounded-[5px]">
+              <img src="/bath.png" alt="" className="w-6 h-6" />
+              <span>1 bathroom</span>
+            </div>
+          </div>
+          <p className="font-bold text-lg mb-[10px]">Nearby places</p>
+          <div className="flex justify-between px-[20px] py-[10px] bg-white rounded-[10px]">
+            <div>
+              <img src="/school.png" alt="" className="w-6 h-6" />
+              <div>
+                <span>School</span>
+                <p>250m away</p>
+              </div>
+            </div>
+            <div>
+              <img src="/pet.png" alt="" className="w-6 h-6" />
+              <div>
+                <span>Bus Stop</span>
+                <p>100m away</p>
+              </div>
+            </div>
+            <div>
+              <img src="/fee.png" alt="" className="w-6 h-6" />
+              <div>
+                <span>Restaurant</span>
+                <p>200m away</p>
+              </div>
+            </div>
+          </div>
+          <p className="font-bold text-lg mb-[10px]">Location</p>
+          <div className="w-full h-[200px]">
+            <Map items={[singlePostData]} />
+          </div>
+          <div>
+            <button>
+              <img src="/chat.png" alt="" className="w-6 h-6" />
+              Send a Message
+            </button>
+            <button>
+              <img src="/save.png" alt="" className="w-6 h-6" />
+              Save the Place
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
