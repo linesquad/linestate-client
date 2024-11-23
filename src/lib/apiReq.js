c34facdb027eb6e4273f0bd7f885ce5f51335de5
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-11-23 17:12:29 +0400

    change url

--- src/lib/apiReq.js
+++ src/lib/apiReq.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiReq = axios.create({
-  baseURL: "http://localhost:8001/api",
+  // baseURL: "http://localhost:8001/api",
+  baseURL: "https://linestatetest-b8bc3b460ebf.herokuapp.com/api",
   withCredentials: true,
 });
 
